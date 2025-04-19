f4796848417fc87ba073b4ee3cef2954bcd003fa
prajwal-gadhave-selldo <prajwal.ghadhave@sell.do>
2025-04-20 00:32:36 +0530

    fix: Feature Section Fixed

--- app/(landing page)/page.tsx
+++ app/(landing page)/page.tsx
@@ -59,17 +59,31 @@ export default function HomePage() {
         id="howItWorks"
         primaryTitle="How"
         secondaryTitle="It Works"
+        className="py-20 font-bold"
       >
         <FeatureGradient />
       </SectionWrapper>
 
-      <SectionWrapper
-        id="scrapingFeatures"
-        primaryTitle="Scraping"
-        secondaryTitle="Features"
-      >
-        <FeatureSection />
-      </SectionWrapper>
+      <div className="relative flex flex-col items-center justify-center bg-[#0C0A09] py-20">
+        <div
+          className={cn(
+            "absolute inset-0",
+            "[background-size:20px_20px]",
+            "[background-image:radial-gradient(#d4d4d4_1px,transparent_1px)]",
+            "dark:[background-image:radial-gradient(#404040_1px,transparent_1px)]"
+          )}
+        />
+        <div className="pointer-events-none absolute inset-0 flex items-center justify-center bg-[#0C0A09] [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+
+        <SectionWrapper
+          id="scrapingFeatures"
+          primaryTitle="Scraping"
+          secondaryTitle="Features"
+          className="min-w-full relative z-20 font-bold"
+        >
+          <FeatureSection />
+        </SectionWrapper>
+      </div>
     </div>
   );
 }
