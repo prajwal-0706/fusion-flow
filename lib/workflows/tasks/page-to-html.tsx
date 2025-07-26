9d4a8fbc272029bfb57763f2a9480ef0beef875e
prajwal-0706 <prajwalgadhave356@gmail.com>
2025-07-26 12:33:30 +0530

    feat: New Task Node added (Page to Html)

---
+++ lib/workflows/tasks/page-to-html.tsx
@@ -0,0 +1,19 @@
+import { CodeIcon, LucideProps } from "lucide-react";
+
+import { TaskParamType, TaskType } from "@/types/task";
+
+export const PageToHtmlTask = {
+  type: TaskType.PAGE_TO_HTML,
+  label: "Get Html from Page",
+  icon: (props: LucideProps) => (
+    <CodeIcon className="stroke-rose-400" {...props} />
+  ),
+  isEntryPoint: false,
+  inputs: [
+    {
+      name: "Web Page",
+      type: TaskParamType.BROWSER_INSTANCE,
+      required: true,
+    },
+  ],
+};
--- lib/workflows/tasks/registry.tsx
+++ lib/workflows/tasks/registry.tsx
@@ -1,6 +1,8 @@
 import { TaskType } from "@/types/task";
 import { LaunchBrowserTask } from "./launch-browser";
+import { PageToHtmlTask } from "./page-to-html";
 
 export const TaskRegistry = {
   [TaskType.LAUNCH_BROWSER]: LaunchBrowserTask,
+  [TaskType.PAGE_TO_HTML]: PageToHtmlTask,
 };
